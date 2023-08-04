8b757102d3d32853eff58483541887520a3bf887
Evince <baneyue@gmail.com>
2023-08-04 15:11:58 +0800

    update dytv cdn

--- plugins/douyu.com/mod.ts
+++ plugins/douyu.com/mod.ts
@@ -3,6 +3,8 @@ import { get_play_url } from './dytv.ts';
 import { play_video, set_term_title } from '@utils/common.ts';
 import { plugin_t } from '@utils/types.ts';
 
+const DEFAULT_CDN_DOMAIN = 'openflv-huos.douyucdn2.cn';
+
 const douyu = new Command()
   .version('0.0.1')
   .description('play douyu live')
@@ -12,7 +14,7 @@ const douyu = new Command()
   .option('-d, --domain <domain:string>', 'cdn domain')
   .action(async (opts, rid) => {
     try {
-      const vi = await get_play_url(rid, !!opts.cdn, opts.bitrate, opts.domain);
+      const vi = await get_play_url(rid, !!opts.cdn, opts.bitrate, opts.domain || DEFAULT_CDN_DOMAIN);
       if (vi.title) set_term_title(vi.title);
       // fix `[ffmpeg] http: Cannot reuse HTTP connection for different host: hlstct.douyucdn2.cn:-1 != 111.19.145.183:-1`
       // see: https://github.com/mpv-player/mpv/issues/5286#issuecomment-415980517
