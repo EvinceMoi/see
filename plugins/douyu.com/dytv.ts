6f3625a36b9a9bf2d2f0fe3839ef705a89909a03
Evince <baneyue@gmail.com>
2023-08-04 18:22:35 +0800

    dytv use pc source as default

--- plugins/douyu.com/dytv.ts
+++ plugins/douyu.com/dytv.ts
@@ -7,17 +7,9 @@ const md5sum = (data: string) => {
   return new Hash('md5').digest(encode(data)).hex();
 }
 
-const RE_KEY = /(\d{1,9}[0-9a-zA-Z]+)_?\d{0,4}p?(\/playlist|.m3u8)/;
+const RE_KEY = /(\d{1,9}[0-9a-zA-Z]+)_?\d{0,4}p?(\/playlist|.m3u8|.flv)/;
 const DID = '10000000000000000000000000001501';
 
-const get_mobile_page = async (rid: string): Promise<string> => {
-  const resp = await fetch(`https://m.douyu.com/${rid}`, {
-    headers: {
-      ...USER_AGENT
-    }
-  });
-  return resp.text();
-}
 interface room_info_t {
   rid: number;
   vipId: number;
@@ -26,6 +18,21 @@ interface room_info_t {
   cate2Name: string;
   isLive: number;
   showTime: number;
+}
+
+const fetch_html = async (url: string): Promise<string> => {
+  const resp = await fetch(url, {
+    headers: {
+      ...USER_AGENT
+    }
+  });
+  return resp.text();
+}
+const get_mobile_page = async (rid: string): Promise<string> => {
+  return await fetch_html(`https://m.douyu.com/${rid}`);
+}
+const get_pc_page = async (rid: string): Promise<string> => {
+  return await fetch_html(`https://www.douyu.com/${rid}`);
 }
 const get_room_info = (html: string): room_info_t => {
   const doc = new DOMParser().parseFromString(html, 'text/html')!;
@@ -89,7 +96,78 @@ const get_stream_key_from_preview = async (rid: string): Promise<stream_info_t> 
   };
 }
 
-const get_stream_key_from_page = async (rid: string, html: string): Promise<stream_info_t> => {
+const get_stream_from_pc_page = async (rid: string, html: string): Promise<stream_info_t> => {
+  const match_func = html.match(/(var vdwdae325w_64we[\s\S]*function ub98484234[\s\S]*?)function/);
+  if (!match_func) {
+    return {
+      error: 'failed to search func',
+    };
+  }
+  const func_ub9 = match_func[1].replace(/eval.*;}/, 'strc;}');
+  const ret = eval(`${func_ub9} ub98484234()`);
+
+  const match_v = ret.match(/v=(\d+)/);
+  if (!match_v) {
+    return {
+      error: 'failed to search v',
+    };
+  }
+
+  const v = match_v[1];
+  const ns = Math.floor(Date.now() / 1000);
+  const rb = md5sum(rid + DID + String(ns) + v);
+
+  let func_sign: string = ret.replace(/return rt;}\);?/, 'return rt;}');
+  func_sign = func_sign.replace('(function (', 'function sign(');
+  func_sign = func_sign.replace('CryptoJS.MD5(cb).toString()', '"' + rb + '"');
+
+  const sign = eval(`${func_sign} sign('${rid}', '${DID}', '${ns}')`);
+  // cdn: cdn: 主线路ws-h5, 备用线路tct-h5
+  // rate: 1流畅, 2高清, 3超清, 4蓝光4M, 0蓝光8M或10M
+  const cdn = `ws-h5`; const rate = `-1`;
+  const params = `${sign}&cdn=${cdn}&rate=${rate}`;
+  const api_url = `https://www.douyu.com/lapi/live/getH5Play/${rid}`;
+  const resp = await fetch(api_url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    },
+    body: params
+  });
+  const res = await resp.json();
+  if (res.error !== 0) {
+    return {
+      error: JSON.stringify(res)
+    };
+  }
+  /*
+    {
+      error: 0,
+      msg: "",
+      data: {
+        room_id: 312407,
+        rtmp_url: "https://stream-suzhou-cmcc-36-150-158-100.edgesrv.com:443/live",
+        rtmp_live: "9999rMfVyoBTjV0x_4000.flv?wsAuth=8866...",
+        ...
+      }
+    }
+   */
+  const { rtmp_url, rtmp_live } = res.data;
+  const url = [rtmp_url, rtmp_live].join('/');
+  const match = rtmp_live.match(RE_KEY);
+  if (!match || match.length != 3) {
+    return {
+      error: 'failed to parse key',
+    };
+  }
+  const [_, key, _suffix] = match;
+  return {
+    key,
+    url,
+  }
+}
+
+const get_stream_from_mobile_page = async (rid: string, html: string): Promise<stream_info_t> => {
   const match_func = html.match(/(function ub98484234.*)\s(var.*)/);
   if (!match_func) {
     return {
@@ -158,7 +236,7 @@ const get_stream_key_from_page = async (rid: string, html: string): Promise<stre
   }
 }
 
-export const get_play_url = async (rid: string, use_cdn: boolean, bitrate = 8000, domain: string | null = null): Promise<video_info_t> => {
+export const get_play_url = async (rid: string, cdn_host: string | null = null): Promise<video_info_t> => {
   const title = (room: room_info_t) => {
     const open_time = moment(room.showTime * 1000).format('YYYY-MM-DD HH:mm:ss');
     return `斗鱼 - ${room.rid}|${room.nickname}|${room.cate2Name} - [${open_time}]`;
@@ -169,20 +247,24 @@ export const get_play_url = async (rid: string, use_cdn: boolean, bitrate = 8000
   if (!room_info.isLive) throw new Error('room is not living at the moment');
   const real_rid = room_info.rid.toString();
 
-  const { error, key, url } = await get_stream_key_from_page(real_rid, html);
-  if (error) throw new Error(`failed to get stream info from mobile page: ${error}`);
-  const steam_title = title(room_info) + ' - ' + key;
-  let video_url = url;
-  if (use_cdn) {
-    let host = new URL(url!).hostname;
-    if (domain) {
-      host = domain;
-    }
-    video_url = `http://${host}/live/${key}_${bitrate}.xs`;
+  let stream_title = title(room_info);
+  let video_url = '';
+  if (cdn_host) {
+    // 使用自定义cdn
+    const { error, key } = await get_stream_from_mobile_page(real_rid, html);
+    if (error) throw new Error(`failed to get stream info from mobile page: ${error}`);
+    stream_title += ' - ' + key;
+    video_url = `http://${cdn_host}/live/${key}_8000.xs`;
+  } else {
+    const page = await get_pc_page(rid);
+    const { error, key, url } = await get_stream_from_pc_page(real_rid, page);
+    if (error) throw new Error(`failed to get stream info from pc page: ${error}`);
+    stream_title += ' - ' + key;
+    video_url = url!;
   }
   
   return {
-    title: steam_title,
+    title: stream_title,
     video: video_url,
   };
 }
--- plugins/douyu.com/mod.ts
+++ plugins/douyu.com/mod.ts
@@ -9,14 +9,13 @@ const douyu = new Command()
   .version('0.0.1')
   .description('play douyu live')
   .arguments('<rid:string>')
-  .option('--no-cdn', 'dont use cdn (use url retrieved from mobile page)')
-  .option('-b, --bitrate <bitrate:number>', 'bitrate', { default: 8000 })
-  .option('-d, --domain <domain:string>', 'cdn domain')
+  .option('--cdn', 'use default cdn')
   .action(async (opts, rid) => {
     try {
-      const vi = await get_play_url(rid, !!opts.cdn, opts.bitrate, opts.domain || DEFAULT_CDN_DOMAIN);
+      const cdn = opts.cdn ? DEFAULT_CDN_DOMAIN : null;
+      const vi = await get_play_url(rid, cdn);
       if (vi.title) set_term_title(vi.title);
-      // fix `[ffmpeg] http: Cannot reuse HTTP connection for different host: hlstct.douyucdn2.cn:-1 != 111.19.145.183:-1`
+      // fix `[ffmpeg] http: Cannot reuse HTTP connection for different host: xxx.douyucdn2.cn:-1 != 111.19.145.183:-1`
       // see: https://github.com/mpv-player/mpv/issues/5286#issuecomment-415980517
       vi.player_options = ['--demuxer-lavf-o-set=http_persistent=0'];
       await play_video(vi);
